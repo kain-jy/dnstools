aca1631f962f35729b77d73a1df0e899c1643249
kain <me@kain-jy.com>
2015-02-23 16:29:33 +0900

    Added dnstools package

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='dnstools',
-    version='0.0.0',
+    version='0.0.1',
+    packages=['dnstools'],
     author='kain',
     author_email='me@kain-jy.com',
     entry_points={
